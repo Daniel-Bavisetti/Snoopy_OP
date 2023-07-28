bffd3596881d8651d83422d1d3b5a6ec73d8f739
priyanka13-gb <98728444+priyanka13-gb@users.noreply.github.com>
2023-07-28 19:39:41 +0530

    Add files via upload

--- acm.js
+++ acm.js
@@ -8,9 +8,74 @@ const editor = grapesjs.init({
     width: 'auto',
 
     storageManager: false,
+    layerManager: {
+        appendTo: '.layers-container'
+    },
 
-    panels: { defaults: [] },
+    panels: {
+        defaults: [{
+            id: 'layers',
+            el: '.panel__right',
 
+            resizable: {
+                maxDim: 350,
+                minDim: 200,
+                tc: 0,
+                cl: 1,
+                cr: 0,
+                bc: 0,
+
+                keyWidth: 'flex-basis',
+            },
+        },
+        {
+            id: 'panel-switcher',
+            el: '.panel__switcher',
+            buttons: [{
+                id: 'show-layers',
+                active: true,
+                label: 'Layers',
+                command: 'show-layers',
+
+                togglable: false,
+            }, {
+                id: 'show-style',
+                active: true,
+                label: 'Styles',
+                command: 'show-styles',
+                togglable: false,
+            }],
+        },
+        {
+            id: 'panel-devices',
+            el: '.panel__devices',
+            buttons: [{
+                id: 'device-desktop',
+                label: 'Desktop',
+                command: 'set-device-desktop',
+                active: true,
+                togglable: false,
+            }, {
+                id: 'device-mobile',
+                label: 'Mobile',
+                command: 'set-device-mobile',
+                togglable: false,
+            }],
+        }
+        ],
+
+    },
+
+    deviceManager: {
+        devices: [{
+            name: 'Desktop',
+            width: '',
+        }, {
+            name: 'Mobile',
+            width: '320px',
+            widthMedia: '480px',
+        }]
+    },
     blockManager: {
         appendTo: '#blocks',
         blocks: [
@@ -49,6 +114,138 @@ const editor = grapesjs.init({
 
                 activate: true,
             }
+
         ]
     },
+    selectorManager: {
+        appendTo: '.styles-container'
+    },
+    styleManager: {
+        appendTo: '.styles-container',
+        sectors: [{
+            name: 'Dimension',
+            open: false,
+
+            buildProps: ['width', 'padding', 'margin', 'height', 'position'],
+
+            properties: [
+                {
+
+                    type: 'integer',
+                    name: 'The width',
+                    property: 'width',
+                    units: ['px', '%'],
+                    defaults: 'auto',
+                    min: 0,
+                }
+            ]
+        }, {
+            name: 'Extra',
+            open: false,
+            buildProps: ['background-color', 'box-shadow', 'custom-prop'],
+            properties: [
+                {
+                    id: 'custom-prop',
+                    name: 'Custom Label',
+                    property: 'font-size',
+                    type: 'select',
+                    defaults: '32px',
+
+                    options: [
+                        { value: '12px', name: 'Tiny' },
+                        { value: '18px', name: 'Medium' },
+                        { value: '32px', name: 'Big' },
+                    ],
+                }
+            ]
+        }]
+    },
 });
+
+
+editor.BlockManager.add('my-block-id', {
+    // ...
+    content: {
+        tagName: 'div',
+        draggable: false,
+        attributes: { 'some-attribute': 'some-value' },
+        components: [
+            {
+                tagName: 'span',
+                content: '<b>Some static content</b>',
+            }, {
+                tagName: 'div',
+
+                components: '<span>HTML at some point</span>',
+            }
+        ]
+    }
+});
+editor.Panels.addPanel({
+    id: 'panel-top',
+    el: '.panel__top',
+});
+editor.Panels.addPanel({
+    id: 'basic-actions',
+    el: '.panel__basic-actions',
+    buttons: [
+        {
+            id: 'visibility',
+            active: true,
+            className: 'btn-toggle-borders',
+            label: '<u>B</u>',
+            command: 'sw-visibility',
+        }, {
+            id: 'export',
+            className: 'btn-open-export',
+            label: 'Exp',
+            command: 'export-template',
+            context: 'export-template',
+        }, {
+            id: 'show-json',
+            className: 'btn-show-json',
+            label: 'JSON',
+            context: 'show-json',
+            command(editor) {
+                editor.Modal.setTitle('Components JSON')
+                    .setContent(`<textarea style="width:100%; height: 250px;">
+              ${JSON.stringify(editor.getComponents())}
+            </textarea>`)
+                    .open();
+            },
+        }
+    ],
+});
+editor.Commands.add('show-layers', {
+    getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
+    getLayersEl(row) { return row.querySelector('.layers-container') },
+
+    run(editor, sender) {
+        const lmEl = this.getLayersEl(this.getRowEl(editor));
+        lmEl.style.display = '';
+    },
+    stop(editor, sender) {
+        const lmEl = this.getLayersEl(this.getRowEl(editor));
+        lmEl.style.display = 'none';
+    },
+});
+editor.Commands.add('show-styles', {
+    getRowEl(editor) { return editor.getContainer().closest('.editor-row'); },
+    getStyleEl(row) { return row.querySelector('.styles-container') },
+
+    run(editor, sender) {
+        const smEl = this.getStyleEl(this.getRowEl(editor));
+        smEl.style.display = '';
+    },
+    stop(editor, sender) {
+        const smEl = this.getStyleEl(this.getRowEl(editor));
+        smEl.style.display = 'none';
+    },
+});
+editor.Commands.add('set-device-desktop', {
+    run: editor => editor.setDevice('Desktop')
+  });
+  editor.Commands.add('set-device-mobile', {
+    run: editor => editor.setDevice('Mobile')
+  });
+
